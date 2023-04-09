72c1f96ebd64fae2b8266ce381d2849e0e152a4c
jawnior <jakub.jawniak@gmail.com>
2023-04-09 20:22:29 +0200

    Fixed bug with the same className as in other file

--- src/components/aboutMe/AboutMe.js
+++ src/components/aboutMe/AboutMe.js
@@ -4,10 +4,10 @@ import './AboutMe.css';
 export function AboutMe() {
   return (
     <div id="about-me">
-        <div className="text-container">
+        <div className="text-container-about-me">
           <h2>O mnie</h2>
         </div>
-          <div className="description">
+          <div className="description-about-me">
             <p>Jestem uprawnionym geodetą z 35 letnim doświadczeniem świadczącym usługi<br/>na terenie całego kraju ze szczególnym uwzględnieniem województwa małopolskiego.</p>
             <p>Zajmuję się między innymi: podziałem nieruchomości, tyczeniem budynków<br/>oraz opracowywaniem map i inwentaryzacją.</p>
             <p>Zapraszam do skorzystania z moich usług popartych długim stażem i nienaruszoną opinią. </p>
